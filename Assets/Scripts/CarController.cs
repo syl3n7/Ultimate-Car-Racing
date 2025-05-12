cb7e2a1d6f024529e665ebbaf6751f6253b2442b
syl3n7 <3891084+syl3n7@users.noreply.github.com>
2025-05-12 17:45:21 +0100

    the car is now working
    i didnt understand that the power factor must be a high number in order for the car to actually drive
    a number like 1000 is close to what we want the car to feel, but needs fine tuning and a power curve

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,318 +1,63 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class CarController : MonoBehaviour
-{
-    [Header("Wheels")]
+
+
+public class CarController : MonoBehaviour {
+
     public Wheel[] wheels;
-    
-    [Header("Engine")]
-    public float motorPower = 1000f;
-    public float brakePower = 3000f;
-    public AnimationCurve powerCurve;
-    
-    [Header("Steering")]
-    public float maxSteerAngle = 30f;
-    public float steerSpeed = 5f;
-    public float wheelbase = 2.5f; // Distance between front and rear wheels
-    public float trackWidth = 1.5f; // Distance between left and right wheels
-    public bool useAckermannSteering = true;
-    
-    [Header("Stability")]
-    public Transform centerOfMass;
-    public float downforceCoefficient = 1.0f;
-    public float antiRollForce = 5000f;
-    
-    [Header("Input")]
     public Vector2 moveInput;
-    private float currentSteerAngle = 0f;
-    private Rigidbody carRigidbody;
-    
-    // Network properties
-    [HideInInspector] public bool IsLocal { get; private set; }
-    [HideInInspector] public string PlayerId { get; private set; }
-    [HideInInspector] public bool HasInputChanges { get; set; }
-    [HideInInspector] public float LastStateTimestamp { get; set; }
-    [HideInInspector] public float LastInputTimestamp { get; set; }
-    
-    public Rigidbody Rigidbody => carRigidbody;
-    public float CurrentThrottle => moveInput.y;
-    public float CurrentSteering => moveInput.x;
-    public float CurrentBrake => (moveInput.y < 0f) ? -moveInput.y : 0f;
+    public float powerMultiplier = 1;
+    public float maxSteer = 30, wheelbase = 2.5f, trackwidth = 1.5f;
 
-    void Awake()
-    {
-        carRigidbody = GetComponent<Rigidbody>();
-        if(centerOfMass != null)
-            carRigidbody.centerOfMass = centerOfMass.localPosition;
-    }
-    
-    public void Initialize(string playerId, bool isLocal)
-    {
-        PlayerId = playerId;
-        IsLocal = isLocal;
-        enabled = true;
-    }
-    
-    void FixedUpdate()
-    {
-        if (wheels == null || wheels.Length == 0)
-        {
-            Debug.LogWarning("No wheels assigned to car controller");
-            return;
-        }
-        
-        // Add debugging output
-        Debug.Log($"Input: {moveInput}, Speed: {carRigidbody.velocity.magnitude} m/s");
-        
-        ApplyMotorTorque();
-        ApplySteering();
-        UpdateWheelVisuals();
-        ApplyDownforce();
-        ApplyAntiRoll();
-    }
-    
-    void ApplyMotorTorque()
-    {
-        float throttleInput = moveInput.y;
-        float brakeInput = (throttleInput < 0) ? -throttleInput : 0;
-        
-        // Get the engine RPM (average of powered wheels)
-        float rpm = 0;
-        int poweredWheels = 0;
-        
-        foreach (var wheel in wheels)
-        {
-            if (wheel.powered)
-            {
-                rpm += wheel.collider.rpm;
-                poweredWheels++;
-            }
-        }
-        
-        if (poweredWheels > 0)
-            rpm /= poweredWheels;
-            
-        // Normalize RPM for power curve (0-1 range)
-        float normalizedRPM = Mathf.Clamp01(Mathf.Abs(rpm) / 3000f);
-        float powerFactor = powerCurve.Evaluate(normalizedRPM);
-        
-        foreach (var wheel in wheels)
-        {
-            // Apply motor torque to powered wheels
-            if (wheel.powered && throttleInput > 0)
-            {
-                wheel.collider.motorTorque = throttleInput * motorPower * powerFactor;
-            }
-            else
-            {
-                wheel.collider.motorTorque = 0;
-            }
-            
-            // Apply brakes
-            if (wheel.hasBrakes && brakeInput > 0)
-            {
-                wheel.collider.brakeTorque = brakeInput * brakePower;
-            }
-            else
-            {
-                wheel.collider.brakeTorque = 0;
-            }
-        }
-    }
-    
-    void ApplySteering()
-    {
-        float targetSteerAngle = moveInput.x * maxSteerAngle;
-        currentSteerAngle = Mathf.Lerp(currentSteerAngle, targetSteerAngle, steerSpeed * Time.fixedDeltaTime);
-        
-        if(useAckermannSteering)
-        {
-            // Apply Ackermann steering geometry
-            float turnRadius = Mathf.Abs(wheelbase / Mathf.Sin(Mathf.Deg2Rad * currentSteerAngle));
-            float ackermannAngleLeft = 0f;
-            float ackermannAngleRight = 0f;
-            
-            if (Mathf.Abs(currentSteerAngle) > 0.1f)
-            {
-                if (currentSteerAngle > 0) // turning right
-                {
-                    ackermannAngleLeft = Mathf.Rad2Deg * Mathf.Atan(wheelbase / (turnRadius + (trackWidth / 2)));
-                    ackermannAngleRight = Mathf.Rad2Deg * Mathf.Atan(wheelbase / (turnRadius - (trackWidth / 2)));
-                }
-                else // turning left
-                {
-                    ackermannAngleLeft = Mathf.Rad2Deg * Mathf.Atan(wheelbase / (turnRadius - (trackWidth / 2)));
-                    ackermannAngleRight = Mathf.Rad2Deg * Mathf.Atan(wheelbase / (turnRadius + (trackWidth / 2)));
-                }
-                ackermannAngleLeft *= Mathf.Sign(currentSteerAngle);
-                ackermannAngleRight *= Mathf.Sign(currentSteerAngle);
-            }
-            
-            foreach (var wheel in wheels)
-            {
-                if (wheel.steerable)
-                {
-                    if (wheel.wheelType == WheelType.FrontLeft)
-                        wheel.collider.steerAngle = ackermannAngleLeft;
-                    else if (wheel.wheelType == WheelType.FrontRight)
-                        wheel.collider.steerAngle = ackermannAngleRight;
-                }
-            }
-        }
-        else
-        {
-            // Simple steering
-            foreach (var wheel in wheels)
-            {
-                if (wheel.steerable)
-                {
-                    wheel.collider.steerAngle = currentSteerAngle;
-                }
-            }
-        }
-    }
-    
-    void UpdateWheelVisuals()
-    {
-        foreach (var wheel in wheels)
-        {
-            if (wheel.wheelMesh != null)
-            {
-                Vector3 position;
-                Quaternion rotation;
-                wheel.collider.GetWorldPose(out position, out rotation);
-                wheel.wheelMesh.position = position;
-                wheel.wheelMesh.rotation = rotation;
-            }
-        }
-    }
-    
-    void ApplyDownforce()
-    {
-        // Simple downforce based on velocity
-        float downforce = carRigidbody.velocity.sqrMagnitude * downforceCoefficient;
-        carRigidbody.AddForce(-transform.up * downforce);
-    }
-    
-    void ApplyAntiRoll()
-    {
-        // Apply anti-roll forces to reduce body roll
-        ApplyAntiRollToAxle(WheelType.FrontLeft, WheelType.FrontRight);
-        ApplyAntiRollToAxle(WheelType.RearLeft, WheelType.RearRight);
-    }
-    
-    void ApplyAntiRollToAxle(WheelType leftType, WheelType rightType)
-    {
-        Wheel leftWheel = GetWheelByType(leftType);
-        Wheel rightWheel = GetWheelByType(rightType);
-        
-        if (leftWheel == null || rightWheel == null)
-            return;
-            
-        WheelHit leftWheelHit, rightWheelHit;
-        bool groundedLeft = leftWheel.collider.GetGroundHit(out leftWheelHit);
-        bool groundedRight = rightWheel.collider.GetGroundHit(out rightWheelHit);
-        
-        float leftTravel = groundedLeft ? leftWheel.collider.transform.InverseTransformPoint(leftWheelHit.point).y - leftWheel.collider.radius : 1.0f;
-        float rightTravel = groundedRight ? rightWheel.collider.transform.InverseTransformPoint(rightWheelHit.point).y - rightWheel.collider.radius : 1.0f;
-        
-        float antiRollForceAmount = (leftTravel - rightTravel) * antiRollForce;
-        
-        if (groundedLeft)
-            carRigidbody.AddForceAtPosition(leftWheel.collider.transform.up * -antiRollForceAmount, leftWheel.collider.transform.position);
-        if (groundedRight)
-            carRigidbody.AddForceAtPosition(rightWheel.collider.transform.up * antiRollForceAmount, rightWheel.collider.transform.position);
-    }
-    
-    Wheel GetWheelByType(WheelType type)
-    {
-        foreach (var wheel in wheels)
-        {
-            if (wheel.wheelType == type)
-                return wheel;
-        }
-        return null;
-    }
-    
-    // Network-related methods
-    public void SetIsLocal(bool isLocal)
-    {
-        IsLocal = isLocal;
-    }
-    
-    public void ApplyRemoteState(GameManager.PlayerStateData stateData, bool teleport = false)
-    {
-        // Will be implemented when we add networking
-    }
-    
-    public void ApplyRemoteInput(GameManager.PlayerInputData inputData)
-    {
-        // Will be implemented when we add networking
-    }
-    
-    public void Respawn(Vector3 position, Quaternion rotation)
-    {
-        carRigidbody.velocity = Vector3.zero;
-        carRigidbody.angularVelocity = Vector3.zero;
-        transform.position = position;
-        transform.rotation = rotation;
+    public void OnMove(InputValue value) {
+        moveInput = value.Get<Vector2>();
     }
 
-    // Updated OnDrawGizmos with more diagnostic info
-    void OnDrawGizmos()
-    {
-        if (!Application.isPlaying)
-            return;
-            
-        if (carRigidbody != null)
-        {
-            // Draw center of mass
-            Gizmos.color = Color.red;
-            Gizmos.DrawSphere(carRigidbody.worldCenterOfMass, 0.1f);
-            
-            // Draw velocity vector
-            Gizmos.color = Color.blue;
-            Gizmos.DrawLine(
-                carRigidbody.worldCenterOfMass, 
-                carRigidbody.worldCenterOfMass + carRigidbody.velocity * 0.2f
-            );
+    void FixedUpdate() {
+        //ToDo: add checks if wheels are present
+        foreach (var wheel in wheels) {
+            wheel.collider.motorTorque = moveInput.y * powerMultiplier;
         }
-        
-        // Draw wheel forces
-        if (wheels != null)
-        {
-            foreach (var wheel in wheels)
-            {
-                if (wheel.collider != null && Application.isPlaying)
-                {
-                    // Get wheel position
-                    Vector3 position;
-                    Quaternion rotation;
-                    wheel.collider.GetWorldPose(out position, out rotation);
-                    
-                    // Draw motor torque
-                    Gizmos.color = Color.green;
-                    float torqueVisualization = wheel.collider.motorTorque / 1000f; // Scale for visualization
-                    Vector3 motorDirection = wheel.collider.transform.right * Mathf.Sign(wheel.collider.rpm);
-                    Gizmos.DrawRay(position, motorDirection * torqueVisualization);
-                    
-                    // Draw wheel load (weight on the wheel)
-                    WheelHit hit;
-                    if (wheel.collider.GetGroundHit(out hit))
-                    {
-                        Gizmos.color = Color.yellow;
-                        float normalForce = hit.force / 5000f; // Scale for visualization
-                        Gizmos.DrawRay(position, Vector3.up * normalForce);
-                        
-                        // Draw friction
-                        Gizmos.color = Color.red;
-                        Gizmos.DrawRay(hit.point, hit.forwardDir * hit.forwardSlip * 0.5f);
-                        Gizmos.color = Color.magenta;
-                        Gizmos.DrawRay(hit.point, hit.sidewaysDir * hit.sidewaysSlip * 0.5f);
-                    }
-                }
+        float steer = moveInput.x * maxSteer;
+        //ToDo: add math lerp or some sort of interpolation
+        if (moveInput.x > 0) {
+            wheels[0].collider.steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelbase / (trackwidth / 2 + Mathf.Tan(Mathf.Deg2Rad * steer) * wheelbase));
+            wheels[1].collider.steerAngle = steer;
+        } else if (moveInput.x < 0) {
+            wheels[0].collider.steerAngle = steer;
+            wheels[1].collider.steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelbase / (-trackwidth / 2 + Mathf.Tan(Mathf.Deg2Rad * steer) * wheelbase));
+        } else {
+            wheels[0].collider.steerAngle = wheels[1].collider.steerAngle = 0;
+        }
+        for (int i = 0; i < wheels.Length; i++) {
+            Quaternion Rot;
+            Vector3 Pos;
+            wheels[i].collider.GetWorldPose(out Pos, out Rot);
+
+            Transform[] ChildTranforms = new Transform[wheels[i].collider.transform.childCount];
+            int index = 0;
+            foreach (var item in ChildTranforms) {
+                wheels[i].collider.transform.GetChild(index).position = Pos;
+                wheels[i].collider.transform.GetChild(index).rotation = Rot;
+                index++;
             }
+
+            //ToDo: this can be used to rotate the brake calipers
+            //wheels[i].collider.transform.localRotation = Quaternion.Euler(0 , wheels[i].collider.transform.rotation.eulerAngles.y, 0);
         }
     }
+
+}
+
+[System.Serializable]
+public class Wheel {
+    public WheelCollider collider;
+    public WheelType wheelType;
+}
+
+
+[System.Serializable]
+public enum WheelType {
+    front, rear
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 using System;
 
@@ -80,8 +81,35 @@ public class GameManager : MonoBehaviour
             CarController carController = localPlayerObject.GetComponent<CarController>();
             if (carController != null)
             {
-                carController.Initialize(localPlayerId, true);
+                // Add Initialize method to CarController
+                InitializeCarController(carController, localPlayerId, true);
                 activePlayers.Add(localPlayerId, carController);
+            }
+        }
+    }
+    
+    // New method to initialize your simplified CarController
+    private void InitializeCarController(CarController controller, string playerId, bool isLocal)
+    {
+        // Add player input component if this is the local player
+        if (isLocal)
+        {
+            // Ensure car has the right input setup
+            PlayerInput playerInput = controller.gameObject.GetComponent<PlayerInput>();
+            if (playerInput == null)
+            {
+                playerInput = controller.gameObject.AddComponent<PlayerInput>();
+                playerInput.actions = Resources.Load<InputActionAsset>("PlayerControls");
+                playerInput.defaultActionMap = "Player";
+            }
+        }
+        else
+        {
+            // Disable input for non-local players
+            PlayerInput playerInput = controller.gameObject.GetComponent<PlayerInput>();
+            if (playerInput != null)
+            {
+                playerInput.enabled = false;
             }
         }
     }
@@ -100,7 +128,101 @@ public class GameManager : MonoBehaviour
                 spawnRotation = spawnPoint.rotation;
             }
             
-            controller.Respawn(spawnPosition, spawnRotation);
+            // Add the Respawn method to CarController
+            RespawnCarController(controller, spawnPosition, spawnRotation);
+        }
+    }
+    
+    // New method to respawn the simplified CarController
+    private void RespawnCarController(CarController controller, Vector3 position, Quaternion rotation)
+    {
+        // Reset the car's position and rotation
+        Rigidbody rb = controller.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+        controller.transform.position = position;
+        controller.transform.rotation = rotation;
+    }
+    
+    // Method to get player state for network synchronization
+    public PlayerStateData GetPlayerState(string playerId)
+    {
+        if (activePlayers.TryGetValue(playerId, out CarController controller))
+        {
+            Rigidbody rb = controller.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                return new PlayerStateData
+                {
+                    playerId = playerId,
+                    position = controller.transform.position,
+                    rotation = controller.transform.rotation,
+                    velocity = rb.linearVelocity,
+                    angularVelocity = rb.angularVelocity,
+                    timestamp = Time.time
+                };
+            }
+        }
+        return null;
+    }
+    
+    // Method to apply remote player state
+    public void ApplyPlayerState(PlayerStateData stateData, bool teleport = false)
+    {
+        if (activePlayers.TryGetValue(stateData.playerId, out CarController controller))
+        {
+            Rigidbody rb = controller.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                if (teleport)
+                {
+                    controller.transform.position = stateData.position;
+                    controller.transform.rotation = stateData.rotation;
+                    rb.linearVelocity = stateData.velocity;
+                    rb.angularVelocity = stateData.angularVelocity;
+                }
+                else
+                {
+                    // Smoothly interpolate position and rotation
+                    controller.transform.position = Vector3.Lerp(controller.transform.position, stateData.position, 0.25f);
+                    controller.transform.rotation = Quaternion.Slerp(controller.transform.rotation, stateData.rotation, 0.25f);
+                    rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, stateData.velocity, 0.25f);
+                    rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, stateData.angularVelocity, 0.25f);
+                }
+            }
+        }
+    }
+    
+    // Method to get player input for network synchronization
+    public PlayerInputData GetPlayerInput(string playerId)
+    {
+        if (activePlayers.TryGetValue(playerId, out CarController controller))
+        {
+            return new PlayerInputData
+            {
+                playerId = playerId,
+                steering = controller.moveInput.x,
+                throttle = controller.moveInput.y > 0 ? controller.moveInput.y : 0,
+                brake = controller.moveInput.y < 0 ? -controller.moveInput.y : 0,
+                timestamp = Time.time
+            };
+        }
+        return null;
+    }
+    
+    // Method to apply remote player input
+    public void ApplyPlayerInput(PlayerInputData inputData)
+    {
+        if (activePlayers.TryGetValue(inputData.playerId, out CarController controller))
+        {
+            // Set the input values directly
+            controller.moveInput = new Vector2(
+                inputData.steering,
+                inputData.throttle - inputData.brake // Combine throttle and brake into y axis
+            );
         }
     }
 }
